a05d0a44ee23f45d108dd1a17560ef5657f8e46f
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2023-05-14 15:43:47 +0300

    added browser router

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Product from './components/Product'
+import { Route, Routes } from "react-router-dom";
+import Cart from './routes/Cart';
+import Home from './routes/Home';
+import About from './routes/About';
+import Contact from './routes/Contact';
 
 function App() {
 
   return (
     <div className="home">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
       <Navbar />
-      <Product icon={""} title={"title"} price={"test price"} />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/cart' element={<Cart />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+      </Routes>
     </div>
   )
 }
