c3191a8b234e6effd998c543841f7739f2419239
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2023-05-14 13:18:23 +0300

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ function App() {
 
   return (
     <div className="home">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <Navbar />
       <Product icon={""} title={"title"} price={"test price"} />
     </div>
