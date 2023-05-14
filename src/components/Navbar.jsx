0f4330b778a5bcd324088c5d086edfc6e57535aa
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2023-05-15 00:39:12 +0300

    fix image overlap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 fixed top-0 left-0 right-0">
+    <nav className="bg-gray-800 fixed top-0 left-0 right-0 z-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
