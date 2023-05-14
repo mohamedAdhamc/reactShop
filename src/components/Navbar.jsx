4b73428fb6869cd4b8e4e6626aee63fbee683a74
mohamedAdhamc <100296264+mohamedAdhamc@users.noreply.github.com>
2023-05-14 13:23:39 +0300

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,23 @@
+
 const Navbar = () => {
-    return (
-        <div id="navbar">Navbar</div>
-    );
-}
- 
+  return (
+    <nav className="bg-gray-800 fixed top-0 left-0 right-0">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex-shrink-0">
+            <a href="/" className="text-white font-bold text-xl">My App</a>
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
+              <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+              <a href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
