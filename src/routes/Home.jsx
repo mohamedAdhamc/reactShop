6bbe137a76edd75bea2c4a7c3fac32e3b6cd13ea
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2023-05-14 18:37:40 +0300

    move products to separate file

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,39 +2,9 @@ import FilterBar from "../components/Filterbar";
 import Navbar from "../components/Navbar";
 import Product from "../components/Product";
 import { useState } from "react";
-
+import products from "../data/products";
 
 const Home = () => {
-    const products = [
-        {
-          id: 1,
-          title: 'Product 1',
-          price: 9.99,
-          imageUrl: 'https://images.pexels.com/photos/4041391/pexels-photo-4041391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-          rating: 4.5,
-          reviews: 10,
-        },
-        {
-          id: 2,
-          title: 'Product 2',
-          price: 74.99,
-          imageUrl: 'https://images.pexels.com/photos/4041391/pexels-photo-4041391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-          description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-          rating: 3.8,
-          reviews: 5,
-        },
-        {
-          id: 3,
-          title: 'Product 3',
-          price: 149.99,
-          imageUrl: 'https://images.pexels.com/photos/4041391/pexels-photo-4041391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-          description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-          rating: 2.9,
-          reviews: 57,
-        },
-        // Add more products as needed
-      ];
     
       const [filters, setFilters] = useState({rating: "all", priceRange: "all", discount: "all"});
     
