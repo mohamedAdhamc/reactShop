013de9a0a9f164afd4405d51c9a9b01958408f70
mohamedAdhamc <100296264+mohamedAdhamc@users.noreply.github.com>
2023-05-15 11:02:56 +0300

    closebutt

--- src/components/cart-page-components/CartItem.jsx
+++ src/components/cart-page-components/CartItem.jsx
@@ -6,14 +6,19 @@ const CartItem = ({ item, id, cartItemIds, setCartItemIds }) => {
   }
 
   return (
-    <div className="flex items-center py-4 border-b border-gray-300">
+    <div className="flex items-start justify-between py-4 border-b border-gray-300">
       <img className="w-16 h-16 rounded" src={item.imageUrl} alt={item.title} />
-      <div className="ml-4">
+      <div className="ml-4 flex-grow">
         <h3 className="font-bold">{item.title}</h3>
         <p className="text-gray-600">Price: ${item.price}</p>
-        <button onClick={removeItem} className="text-red-600 bg-transparent">X</button>
         {/* Render other item details like quantity, total price, etc. */}
       </div>
+      <button
+        onClick={removeItem}
+        className="text-red-600 bg-transparent hover:text-black hover:border-black rounded p-1 transition-all duration-300"
+      >
+        X
+      </button>
     </div>
   );
 };
