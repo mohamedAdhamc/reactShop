5ce0aa7e4922020f65692d862598094ded12e689
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2023-05-15 10:58:12 +0300

    more dark mode fixes

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -2,7 +2,7 @@ import FeedbackForm from "../components/FeedbackForm";
 
 const Contact = () => {
     return (
-        <div className="contact-page bg-gray-100 w-[50vw] px-7 py-5 rounded-[2vw]">
+        <div className="contact-page bg-gray-100 w-[50vw] px-7 py-5 rounded-[2vw] dark:text-gray-700">
             <h1>Contact us!</h1>
             <FeedbackForm />
         </div>
