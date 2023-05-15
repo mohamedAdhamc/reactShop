75a143ff3fd424fa2162eab2f4fee5a324cbb74d
mohamedAdhamc <100296264+mohamedAdhamc@users.noreply.github.com>
2023-05-15 11:44:14 +0300

    dd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,15 @@ const Navbar = () => {
     <nav className="bg-gray-800 fixed top-0 left-0 right-0 z-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <div className="flex-shrink-0">
-            <Link to="/" className="text-white font-bold text-xl hover:text-white">My App</Link>
+          <div className="flex min-w-100">
+              <div className="flex">
+              <img
+                  className="w-8 h-8 mr-2"
+                  src="https://cdn-icons-png.flaticon.com/512/6007/6007783.png"
+                  alt="Icon"
+                />
+                <Link to="/" className="text-white font-bold text-xl hover:text-white">Bargain Max</Link>
+              </div>
           </div>
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-4">
