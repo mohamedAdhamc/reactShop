fb2e2f11bb4b6edce917cc7b301e6ac45f766456
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2023-05-15 10:55:20 +0300

    dark mode fixes

--- src/components/cart-page-components/CartItem.jsx
+++ src/components/cart-page-components/CartItem.jsx
@@ -9,7 +9,7 @@ const CartItem = ({ item, id, cartItemIds, setCartItemIds }) => {
     <div className="flex items-center py-4 border-b border-gray-300">
       <img className="w-16 h-16 rounded" src={item.imageUrl} alt={item.title} />
       <div className="ml-4">
-        <h3 className="font-bold">{item.title}</h3>
+        <h3 className="font-bold dark:text-gray-700">{item.title}</h3>
         <p className="text-gray-600">Price: ${item.price}</p>
         <button onClick={removeItem} className="text-red-600 bg-transparent">X</button>
         {/* Render other item details like quantity, total price, etc. */}
